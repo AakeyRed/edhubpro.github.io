715d4a5a7ef5105ee832ac303e071e3706e13a86
AakeyRed <aakashdhingrolia0741@gmail.com>
2021-09-26 23:51:42 +0530

    slider

--- custom/js/script.js
+++ custom/js/script.js
@@ -49,7 +49,7 @@ $(document).ready(function () {
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 1,
                     slidesToScroll: 1
                 }
             },
